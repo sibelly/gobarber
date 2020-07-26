135285b103d559ac6d55834c70d94e81ecceff9a
sibelly <sibellychan@hotmail.com>
2020-07-25 22:18:53 -0400

    Adds validation ux on sign in and sign up pages

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
@@ -12,17 +12,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
       const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
       await schema.validate(data, {
@@ -36,10 +36,13 @@ const SignIn: React.FC = () => {
 
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
+
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="Email" />
           <Input
             name="password"
@@ -48,45 +51,15 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
 
-          <Button type="submit"> Entrar </Button>
-
-          <a href="forgot"> Esqueci minha senha</a>
+          <Button type="submit"> Cadastrar </Button>
         </Form>
         <a href="login">
-          <FiLogIn />
-          Criar conta
+          <FiArrowLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
 
-// const SignIn: React.FC = () => (
-//   <Container>
-//     <Content>
-//       <img src={logoImg} alt="GoBarber" />
-//       <Form onSubmit={() => {}}>
-//         <h1>Faça seu logon</h1>
-//         <Input name="email" icon={FiMail} placeholder="Email" />
-//         <Input
-//           name="password"
-//           icon={FiLock}
-//           type="password"
-//           placeholder="Senha"
-//         />
-
-//         <Button type="submit"> Entrar </Button>
-
-//         <a href="forgot"> Esqueci minha senha</a>
-//       </Form>
-//       <a href="login">
-//         <FiLogIn />
-//         Criar conta
-//       </a>
-//     </Content>
-//     <Background />
-//   </Container>
-// );
-
-export default SignIn;
+export default SignUp;
