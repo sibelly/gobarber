58a2dea60d66d26f1579304d8a6e1c7af0ed8df1
Sibelly <sibellychan@hotmail.com>
2021-02-10 11:05:09 -0400

    Adiciona validação nas rotas com celebrate

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/container';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
