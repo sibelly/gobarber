9e7bc15c368d47c58b490f2979bed23bde26f91f
Sibelly <sibellychan@hotmail.com>
2021-03-16 10:27:29 -0400

    Adiciona tela para recuperacao de senha e de esqueci senha

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,13 +6,17 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
-    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
