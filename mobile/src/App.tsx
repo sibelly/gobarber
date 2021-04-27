82927ae1c364dcb6301578b74f3d15fce1f36748
Sibelly <sibellychan@hotmail.com>
2021-04-26 23:13:09 -0400

    Finaliza header do dashboard e adiciona theme provider

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -3,19 +3,24 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { ThemeProvider } from 'styled-components';
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
+import defaultTheme from './styles/theme/default';
+
 const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
+  <ThemeProvider theme={defaultTheme}>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  </ThemeProvider>
 );
 
 export default App;
